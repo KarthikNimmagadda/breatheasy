ca6fbe2a9e19c1ef1f074c24a5b6c4ce3d39e7c8
Karthik Nimmagadda <karthiknimmagadda@Karthiks-MacBook-Pro.local>
2023-01-08 08:33:34 -0800

    Updates

---
+++ precache-manifest.872df59dda1a43c04befe1c0421312b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a34068e824afeccd13e",
+    "url": "/breatheasy/static/css/main.468926e9.chunk.css"
+  },
+  {
+    "revision": "0a34068e824afeccd13e",
+    "url": "/breatheasy/static/js/main.0a34068e.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/breatheasy/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "ade9cbb720838f3eacc7",
+    "url": "/breatheasy/static/js/2.ade9cbb7.chunk.js"
+  },
+  {
+    "revision": "980dd75550711a883cf9",
+    "url": "/breatheasy/static/js/runtime~main.980dd755.js"
+  },
+  {
+    "revision": "94a1828c6a482ae4161bea844ed20d8e",
+    "url": "/breatheasy/index.html"
+  }
+];
